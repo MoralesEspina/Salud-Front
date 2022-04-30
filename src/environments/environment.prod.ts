3b27a1f531d00081763c67ff64c94ebc61295fbf
MoralesEspina <60724084+MoralesEspina@users.noreply.github.com>
2022-04-29 19:07:55 -0600

    Utilizando Back Nuestro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  URL: 'https://das-jalapa-system.herokuapp.com/das/v1',
-  URL2: 'https://das-jalapa-system.herokuapp.com',
-  // URL: 'http://localhost:4000/das/v1',
-  // URL2: 'http://localhost:4000',
+  //URL: 'https://das-jalapa-system.herokuapp.com/das/v1',
+  //URL2: 'https://das-jalapa-system.herokuapp.com',
+   URL: 'https://proyecto-salud-back.herokuapp.com/das/v1',
+   URL2: 'https://proyecto-salud-back.herokuapp.com/',
   URLCustomForms: 'https://das-custom-forms.herokuapp.com',
   // URLCustomForms: 'http://localhost:3000',
   SECRET_KEY: '3xmz8DaVvcGzz41bVcrq8ImKE8EwPTlXjUNaLOxqsb00YcT9RhV5rkUTmjgw7NJ3FjexDaZ34tZfBrtAAF0RgxUzBFmfZoWjzOaNzBJMdXmjqFeDUAUFltZdxR5anA0p'

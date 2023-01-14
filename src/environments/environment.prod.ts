e7a470dd05fcfb9b87c45f42f2383254aa285028
Dylan Alexis Tobar Orellana <dylantobar2001@gmail.com.com>
2023-01-14 16:35:39 -0600

    cambios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,8 +6,8 @@ export const environment = {
    //URL2: 'https://proyecto-salud-back.herokuapp.com/',
    URL2: 'http://localhost:4000',
     URL: 'http://localhost:4000/das/v1',
-  URLCustomForms: 'https://localhost:3000',
-  //URLCustomForms: 'https://das-custom-forms.herokuapp.com',
+  //URLCustomForms: 'https://localhost:3000',
+  URLCustomForms: 'https://das-custom-forms.herokuapp.com',
   viewSalary: false,
   // URLCustomForms: 'http://localhost:3000',
   SECRET_KEY: '3xmz8DaVvcGzz41bVcrq8ImKE8EwPTlXjUNaLOxqsb00YcT9RhV5rkUTmjgw7NJ3FjexDaZ34tZfBrtAAF0RgxUzBFmfZoWjzOaNzBJMdXmjqFeDUAUFltZdxR5anA0p'

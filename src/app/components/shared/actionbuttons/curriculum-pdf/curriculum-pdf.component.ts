aee782d60a84bc910a71d893e9df13264232f13c
DESKTOP-QGSO4V9\Esdras <moralesespina92@gmail.com>
2023-01-31 18:38:45 -0600

    Arreglos en la impresion del PDF para el curriculum

--- src/app/components/shared/actionbuttons/curriculum-pdf/curriculum-pdf.component.ts
+++ src/app/components/shared/actionbuttons/curriculum-pdf/curriculum-pdf.component.ts
@@ -6,17 +6,18 @@ import { AuthorizationService } from 'src/app/services/authorization.service';
 import { CurriculumService } from 'src/app/services/curriculum.service';
 import { PersonService } from 'src/app/services/person.service';
 import { Curriculum } from 'src/app/utils/reports/pdf';
-import { Img, PdfMakeWrapper, Table, Txt } from 'pdfmake-wrapper';
+import { Cell, Img, PdfMakeWrapper, Table, Txt } from 'pdfmake-wrapper';
 import { UserService } from 'src/app/services/user.service';
 import * as dayjs from 'dayjs';
 import { EducationI } from 'src/app/models/personEducation.model';
 import { ExperienceI } from 'src/app/models/experience.model';
 import { AuthorizationConfigurationFile } from 'src/app/models/authorizationConfiguration';
+import { LocalService } from 'src/app/services/local.service';
 
 type TableRowFam = [string, string, string, string];
 type TableRowPer = [string, string, string, string, string];
 type TableRowEducation = [string, string, string, string, string, string, string];
-type TableRowExperience = [string, string, string, string, string, string, string, string, string];
+type TableRowExperience = [string, string, string, string, string, string, string, string];
 
 @Component({
   selector: 'app-curriculum-pdf',
@@ -34,12 +35,15 @@ export class CurriculumPDFComponent implements OnInit {
   tableRefPer: ReferenceI[] = [];
   tableEducation: EducationI[] = [];
   tableExperience: ExperienceI[] = [];
+  tableExp: any;
   imageURL: string = 'https://firebasestorage.googleapis.com/v0/b/das-jalapa.appspot.com/o/certify%2FFormato_Curriculum_Logo.png?alt=media&token=77b83778-621f-490a-bf0b-601ccc01339d'
+  urlImage = this.localService.getJsonValue('avatar');
 
   constructor(private personService: PersonService,
     private curriculumService: CurriculumService,
     private constancyService: AuthorizationService,
-    private userService: UserService) { }
+    private userService: UserService,
+    private localService: LocalService) { }
 
   ngOnInit() {
     this.loadExperience();
@@ -99,7 +103,8 @@ export class CurriculumPDFComponent implements OnInit {
     if (id_entrada) {
       this.curriculumService.GetExperience(id_entrada).subscribe(
         data => {
-          this.tableExperience = data['data'];
+          this.tableExp = data['data'];
+          console.log(this.tableExp)
         }
       )
     }
@@ -119,6 +124,8 @@ export class CurriculumPDFComponent implements OnInit {
     })
 
     pdf.add(await new Img(this.imageURL).alignment('center').width(200).height(100).relativePosition(0, -60).build())
+
+    pdf.add(await new Img(this.urlImage).alignment('right').width(100).height(100).relativePosition(0, -50).build())
 
     pdf.add(new Txt('Datos generales_____________________________________________').alignment('left').relativePosition(0, 40).color('#2778B4').fontSize(16).bold().end)
 
@@ -178,17 +185,91 @@ export class CurriculumPDFComponent implements OnInit {
     pdf.add(new Txt('NIT').alignment('left').relativePosition(0, 300).bold().end)
     pdf.add(new Txt(this.curriculum.nit).alignment('left').relativePosition(125, 300).end)
 
-    pdf.add(new Txt('Información familiar________________________________________').alignment('center').relativePosition(0, 370).fontSize(16).color('#2778B4').bold().end)
+    pdf.add(new Txt('Información familiar________________________________________').alignment('center').relativePosition(0, 340).fontSize(16).color('#2778B4').bold().end)
     pdf.add(this.createTableRefFam(refFam))
 
-    pdf.add(new Txt('Educación__________________________________________________').alignment('center').relativePosition(0, 500).fontSize(16).color('#2778B4').bold().end)
+    pdf.add(new Txt('Educación__________________________________________________').alignment('center').relativePosition(0, 470).fontSize(16).color('#2778B4').bold().end)
     pdf.add(this.createTableEducation(education))
 
-    pdf.add(new Txt('Referencias Personales_______________________________________').alignment('center').relativePosition(0, 10).fontSize(16).color('#2778B4').bold().pageBreak('before').end)
+    pdf.add(new Txt('Referencias Personales_______________________________________').alignment('center').relativePosition(0, 600).fontSize(16).color('#2778B4').bold().end)
     pdf.add(this.createTableRefPer(refPer))
 
-    pdf.add(new Txt('Experiencia Laboral_______________________________________').alignment('center').relativePosition(0, 140).fontSize(16).color('#2778B4').bold().end)
-    pdf.add(this.createTableExperience(experience))
+    pdf.add(new Txt('Experiencia Laboral_______________________________________').alignment('center').relativePosition(0, 30).fontSize(16).color('#2778B4').bold().pageBreak('before').end)
+      pdf.add(new Table([
+        [new Cell(new Txt('Nombre de la institución o empresa: ' + this.tableExp[0].direction).end).colSpan(4).color('white').fontSize(11).end, null, null, null],
+        [new Cell(new Txt('Dirección: ' + this.tableExp[0].direction).end).colSpan(4).fontSize(11).end, null, null, null],
+        [new Cell(new Txt('Telefono: ' + this.tableExp[0].phone).end).colSpan(2).fontSize(11).end, null, new Cell(new Txt('Nombre jefe inmediato: ' + this.tableExp[0].bossname).end).colSpan(2).fontSize(10).end,null],
+        [new Cell(new Txt('Motivo del retiro: ' + this.tableExp[0].reason).end).colSpan(2).fontSize(11).end, null, new Cell(new Txt('Sector: ' + this.tableExp[0].sector).end).colSpan(2).fontSize(11).end,null],
+        [new Cell(new Txt('Fecha del empleo: ' + this.tableExp[0].dateof + ' al ' + this.tableExp[0].dateto).end).colSpan(2).fontSize(10).end, null, new Cell(new Txt('Salario: Q.' + this.tableExp[0].salary).end).colSpan(2).fontSize(11).end,null],
+        [new Cell(new Txt('Puesto desempeñado: ' + this.tableExp[0].job).end).colSpan(4).fontSize(11).end, null, null, null],
+    ]).layout({
+      vLineColor: () => '#2778B4',
+      hLineColor: () => '#2778B4',
+      fillColor: (rowIndex) => {
+        if (rowIndex === 0) {
+          return '#2778B4';
+        }
+    }
+    })
+    .relativePosition(0, 60)
+    .fontSize(12)
+    .widths("*")
+    .end)
+
+    if (this.tableExp.length >= 2) {
+      pdf.add(new Table([
+        [new Cell(new Txt('Nombre de la institución o empresa: ' + this.tableExp[1].direction).end).colSpan(4).color('white').fontSize(11).end, null, null, null],
+        [new Cell(new Txt('Dirección: ' + this.tableExp[1].direction).end).colSpan(4).fontSize(11).end, null, null, null],
+        [new Cell(new Txt('Telefono: ' + this.tableExp[1].phone).end).colSpan(2).fontSize(11).end, null, new Cell(new Txt('Nombre jefe inmediato: ' + this.tableExp[1].bossname).end).colSpan(2).fontSize(10).end,null],
+        [new Cell(new Txt('Motivo del retiro: ' + this.tableExp[1].reason).end).colSpan(2).fontSize(11).end, null, new Cell(new Txt('Sector: ' + this.tableExp[1].sector).end).colSpan(2).fontSize(11).end,null],
+        [new Cell(new Txt('Fecha del empleo: ' + this.tableExp[1].dateof + ' al ' + this.tableExp[1].dateto).end).colSpan(2).fontSize(10).end, null, new Cell(new Txt('Salario: Q.' + this.tableExp[1].salary).end).colSpan(2).fontSize(11).end,null],
+        [new Cell(new Txt('Puesto desempeñado: ' + this.tableExp[1].job).end).colSpan(4).fontSize(11).end, null, null, null],
+    ]).layout({
+      vLineColor: () => '#2778B4',
+      hLineColor: () => '#2778B4',
+      fillColor: (rowIndex) => {
+        if (rowIndex === 0) {
+          return '#2778B4';
+        }
+    }
+    })
+    .relativePosition(0, 180)
+    .fontSize(12)
+    .widths("*")
+    .end)
+    }
+
+    if (this.tableExp.length >= 3) {
+      pdf.add(new Table([
+        [new Cell(new Txt('Nombre de la institución o empresa: ' + this.tableExp[1].direction).end).colSpan(4).color('white').fontSize(11).end, null, null, null],
+        [new Cell(new Txt('Dirección: ' + this.tableExp[2].direction).end).colSpan(4).fontSize(11).end, null, null, null],
+        [new Cell(new Txt('Telefono: ' + this.tableExp[2].phone).end).colSpan(2).fontSize(11).end, null, new Cell(new Txt('Nombre jefe inmediato: ' + this.tableExp[2].bossname).end).colSpan(2).fontSize(10).end,null],
+        [new Cell(new Txt('Motivo del retiro: ' + this.tableExp[2].reason).end).colSpan(2).fontSize(11).end, null, new Cell(new Txt('Sector: ' + this.tableExp[2].sector).end).colSpan(2).fontSize(11).end,null],
+        [new Cell(new Txt('Fecha del empleo: ' + this.tableExp[2].dateof + ' al ' + this.tableExp[2].dateto).end).colSpan(2).fontSize(10).end, null, new Cell(new Txt('Salario: Q.' + this.tableExp[2].salary).end).colSpan(2).fontSize(11).end,null],
+        [new Cell(new Txt('Puesto desempeñado: ' + this.tableExp[2].job).end).colSpan(4).fontSize(11).end, null, null, null],
+    ]).layout({
+      vLineColor: () => '#2778B4',
+      hLineColor: () => '#2778B4',
+      fillColor: (rowIndex) => {
+        if (rowIndex === 0) {
+          return '#2778B4';
+        }
+    }
+    })
+    .relativePosition(0, 300)
+    .fontSize(12)
+    .widths("*")
+    .end)
+    }
+
+    if (this.tableExp.length <= 1){
+
+    }else if (this.tableExp.length <= 2){
+
+    }else if(this.tableExp.length <= 3){
+
+    }
+
     pdf.create().open();
   }
 
@@ -197,7 +278,7 @@ export class CurriculumPDFComponent implements OnInit {
       [ new Txt('Nombre').bold().color('white').alignment('center').end, new Txt('Teléfono').bold().color('white').alignment('center').end, new Txt('Parentesco').bold().color('white').alignment('center').end, new Txt('Fecha de nacimiento').bold().color('white').alignment('center').end],
       ...this.extractDataToRowFam(refFam)
     ])
-    .relativePosition(0, 390)
+    .relativePosition(0, 370)
     .widths(['*', 90, 90, 110])
       .layout({
         vLineColor: () => '#2778B4',
@@ -206,7 +287,7 @@ export class CurriculumPDFComponent implements OnInit {
           if (rowIndex === 0) {
             return '#2778B4';
           }
-      }
+        }
       })
       .alignment('center')
       .end;
@@ -229,7 +310,7 @@ export class CurriculumPDFComponent implements OnInit {
       ],
       ...this.extractDataToRowEducation(education)
     ])
-    .relativePosition(0, 520)
+    .relativePosition(0, 500)
     .widths([55,90,60,60,50,45,45])
       .layout({
         vLineColor: () => '#2778B4',
@@ -254,7 +335,7 @@ export class CurriculumPDFComponent implements OnInit {
       [ new Txt('Nombre').bold().color('white').alignment('center').end, new Txt('Teléfono').bold().color('white').alignment('center').end, new Txt('Profesión').bold().color('white').alignment('center').end, new Txt('Empresa').bold().color('white').alignment('center').end, new Txt('Parentesco').bold().color('white').alignment('center').end],
       ...this.  extractDataToRowPer(refPer)
     ])
-    .relativePosition(0, 30)
+    .relativePosition(0, 630)
     .widths(['*','*','*','*','*'])
       .layout({
         vLineColor: () => '#2778B4',
@@ -273,21 +354,24 @@ export class CurriculumPDFComponent implements OnInit {
     return refPer.map((row, index) => [row.name, row.phone, row.profession, row.company, row.relationship])
   }
 
+  /*pdf.add(new Table([
+    [ new Txt('Pais').bold().color('black').alignment('center').end, new Txt('Pais').bold().color('white').alignment('center').end],
+    [ 'header 1.1', 'Header 2.1']
+]).headerRows(2).end)*/
 
   createTableExperience(experience: ExperienceI[]): ITable {
     return new Table([
-      [ new Txt('Dirección').bold().color('white').alignment('center').end,
+       [new Txt('Dirección').bold().color('white').alignment('center').end,
        new Txt('Telefono').bold().color('white').alignment('center').end,
        new Txt('Motivo del Retiro').bold().color('white').alignment('center').end,
-       new Txt('Fecha de Inicio').bold().color('white').alignment('center').end,
-       new Txt('Fecha del Final').bold().color('white').alignment('center').end,
+       new Txt('Fecha de empleo').bold().color('white').alignment('center').end,
        new Txt('Puesto Desempeñado').bold().color('white').alignment('center').end,
        new Txt('Nombre del Jefe Inmediato').bold().color('white').alignment('center').end,
        new Txt('Sector').bold().color('white').alignment('center').end,
        new Txt('Salario').bold().color('white').alignment('center').end],
       ...this.extractDataToRowExperience(experience)
     ])
-    .relativePosition(0, 160)
+    .relativePosition(0, 60)
       .layout({
         vLineColor: () => '#2778B4',
         hLineColor: () => '#2778B4',
@@ -302,7 +386,7 @@ export class CurriculumPDFComponent implements OnInit {
   }
 
   extractDataToRowExperience(experience: ExperienceI[]): TableRowExperience[] {
-    return experience.map((row, index) => [row.direction, row.phone, row.reason,  dayjs(row.dateof).format('YYYY'), dayjs(row.dateto).format('YYYY'), row.job, row.bossname, row.sector, row.salary])
+    return experience.map((row, index) => [row.direction, row.phone, row.reason,  dayjs(row.dateof).format('DD/MM/YYYY') + ' al ' + dayjs(row.dateto).format('DD/MM/YYYY'), row.job, row.bossname, row.sector, row.salary])
   }
 
 
